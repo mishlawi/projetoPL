57c2f878ae19ab93a92682130c76e1f91b3e9739
DomBarata <tiagobarata98@gmail.com>
2021-04-01 13:40:20 +0100

    Divisao do trabalho em 2 ficheiros: 1 de escrita em HTML e outro para tratar o train.txt

---
+++ Enunciado5/html.py
@@ -0,0 +1,246 @@
+import os
+
+
+def makeDirs():
+    dir0 = './HTML'
+    dir1 = './HTML/categorias'
+    dir2 = './HTML/styles'
+    if (os.path.exists(dir0) == False):
+        os.mkdir(dir0)
+    if (os.path.exists(dir1) == False):
+        os.mkdir(dir1)
+    if (os.path.exists(dir2) == False):
+        os.mkdir(dir2)
+
+
+def htmlStyle():
+	htmlstyle = open(rf'HTML/styles/style.css',"w")
+	htmlstyle.write(r'''html {
+    font-size: 14px;
+    font-family: 'Open Sans', sans-serif;
+}
+
+
+h1 {
+    font-size: 60px;
+    text-align: center;
+}
+
+p, li {
+    font-size: 16px;
+    line-height: 2;
+    letter-spacing: 1px;
+}
+
+table {
+    padding-top: 5px;
+    text-align: center;
+}
+
+tr:nth-child(even) {background-color: #75E6DA;}
+
+td{
+    font-size: 16px;
+    border: 1px solid black;
+    padding: 15px;
+}
+
+th{
+    font-size: 20px; 
+    text-align: left;
+    height: 50%;
+    background-color: #05445E; 
+    color: white;
+}
+
+html {
+    background-color: #05445E;
+}
+
+body {
+    width: 1200px;
+    margin: 0 auto;
+    background-color: #75E6DA;
+    padding: 0 20px 20px 20px;
+    border: 5px solid black;
+}
+
+h1 {
+    margin: 0;
+    padding: 20px 0;
+    color: #00539F;
+    text-shadow: 3px 3px 1px black;
+}
+
+/* Add a black background color to the top navigation bar */
+.topnav {
+    overflow: hidden;
+    background-color: #e9e9e9;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+    float: left;
+    display: block;
+    color: black;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+    background-color: #ddd;
+    color: black;
+}
+
+/* Style the "active" element to highlight the current page */
+.topnav a.active {
+    background-color: #2196F3;
+    color: white;
+}
+
+/* Style the search box inside the navigation bar */
+.topnav input[type=text] {
+    float: right;
+    padding: 6px;
+    border: none;
+    margin-top: 8px;
+    margin-right: 16px;
+    font-size: 17px;
+}
+
+/* When the screen is less than 600px wide, stack the links and the search field vertically instead of horizontally */
+@media screen and (max-width: 600px) {
+    .topnav a, .topnav input[type=text] {
+        float: none;
+        display: block;
+        text-align: left;
+        width: 100%;
+        margin: 0;
+        padding: 14px;
+    } 
+    .topnav input[type=text] {
+        border: 1px solid #ccc;
+    }
+} ''')
+
+
+def htmlHome(dic):
+    htmlfirst = open("HTML/website.html","w")
+
+    htmlfirst.write("""<!DOCTYPE html>
+<html>
+    <head>
+        <title>Enunciado 5</title>
+        <meta charset="UTF-8"/>
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+        <link href="styles/style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <h1> Enunciado 5, Trabalho Prático de Processamento de Linguagens</h1>
+        <p> Realizado por:</p>
+        <p> Duarte Oliveira A85517</p>
+        <p> Tiago Barata A81195</p>
+        <p> Simão Oliveira A57041</p>
+        <hr>""")
+
+    for elem in dic:
+        htmlfirst.write(
+            rf"""           
+        <h2>{elem}</h2>
+            <p>nº de elementos nesta categoria: {len(dic[elem])} </p>""")
+        
+        htmlfirst.write(
+        rf"""
+            <a href="categorias/{elem}Info.html"> Info {elem}</a>
+            <br>
+            <a href="categorias/{elem}InfoSRepeticao.html"> Info {elem} sem repetidos </a>
+            <hr>""")
+    htmlfirst.write(r"""
+    </body>
+</html>""")
+
+
+def htmlRepetidos(dic, repetidos):
+    for elem in dic:
+        htmln = open(rf'HTML/categorias/{elem}Info.html',"w")
+        htmln.write(rf"""
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+        <link href="../styles/style.css" rel="stylesheet" type="text/css">
+        <title>Informação {elem} </title>""")
+        htmln.write(r"""
+        <meta charset="UTF-8"/>
+    </head>
+    <body>
+        <div class="topnav">
+        <a class="active" href="../website.html"> Home </a>
+        </div>""")
+
+        htmln.write(rf"""
+        <h1>Lista de elementos da categoria {elem}</h1>
+        <table>
+            <tr>
+                <th>Elementos</th>
+                <th>Linha da ocorrência</th>
+            </tr>""")
+
+        for listado, nrs in zip(dic[elem],repetidos[elem]):
+            htmln.write(rf"""
+            <tr>
+                <td>{listado}</td>
+                <td>{nrs}</td>
+            </tr>""")
+        
+        htmln.write(rf"""
+        </table>
+    </body>
+</html>""")
+
+
+def htmlSemRepetidos(dic, semRepetidos):
+    for elem in dic:
+        htmlsemsepetidos = open(rf"HTML/categorias/{elem}InfoSRepeticao.html","w")
+        htmlsemsepetidos.write(rf"""
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+        <link href="../styles/style.css" rel="stylesheet" type="text/css">
+        <title>Informação sem repetidos de {elem} </title>""")
+        
+        htmlsemsepetidos.write(r"""
+        <meta charset="UTF-8"/>
+    </head>
+    <body>
+        <div class="topnav">
+            <a class="active" href="../website.html"> Home </a>
+        </div>""")
+
+        htmlsemsepetidos.write(rf"""
+        <h1>Lista de elementos da categoria {elem}</h1>
+        <table>
+            <tr>
+               <th>Elementos</th>
+               <th>Linha da ocorrência</th>
+            </tr>""")
+
+        for categoria in semRepetidos[elem]:
+            tam = len(semRepetidos[elem][categoria])+1
+            htmlsemsepetidos.write(rf"""
+            <td rowspan={tam}>{categoria}</td>""")
+
+            for ocorrencia in semRepetidos[elem][categoria]:
+                htmlsemsepetidos.write(rf"""
+            <tr>
+                <td>{ocorrencia}</td>
+            </tr>""")
+
+        htmlsemsepetidos.write(rf"""
+        </table>
+    </body>
+</html>""")
--- Enunciado5/lvl5.py
+++ Enunciado5/lvl5.py
@@ -1,5 +1,7 @@
 import re
 import webbrowser
+import html
+
 
 def alpha(lista): 
 	info = {}
@@ -16,6 +18,7 @@ def alpha(lista):
 			typo = []
 		n+=1
 	return info
+
 
 #key -> categoria v: [nr da ocorrencia]
 def organizer(lista):
@@ -83,260 +86,22 @@ def beta (dic, info):
 	return semRepetidos
 
 
-#HTML FUNCTIONS
-
-def htmlStyle():
-	htmlstyle = open(rf'styles/style.css',"w")
-	htmlstyle.write(r'''html {
-  font-size: 14px;
-  font-family: 'Open Sans', sans-serif;
-}
-
-
-h1 {
-  font-size: 60px;
-  text-align: center;
-}
-
-p, li {
-  font-size: 16px;
-  line-height: 2;
-  letter-spacing: 1px;
-}
-
-table {
-  padding-top: 5px;
-  text-align: center;
-}
-
-tr:nth-child(even) {background-color: #75E6DA;}
-
-td{
-  font-size: 16px;
-  border: 1px solid black;
-  padding: 15px;
-}
-
-th{
-  font-size: 20px; 
-  text-align: left;
-  height: 50%;
-  background-color: #05445E; 
-  color: white;
-}
-
-html {
-  background-color: #05445E;
-}
-
-body {
-  width: 1200px;
-  margin: 0 auto;
-  background-color: #75E6DA;
-  padding: 0 20px 20px 20px;
-  border: 5px solid black;
-}
-
-h1 {
-  margin: 0;
-  padding: 20px 0;
-  color: #00539F;
-  text-shadow: 3px 3px 1px black;
-}
-
-/* Add a black background color to the top navigation bar */
-.topnav {
-  overflow: hidden;
-  background-color: #e9e9e9;
-}
-
-/* Style the links inside the navigation bar */
-.topnav a {
-  float: left;
-  display: block;
-  color: black;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-/* Change the color of links on hover */
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
-
-/* Style the "active" element to highlight the current page */
-.topnav a.active {
-  background-color: #2196F3;
-  color: white;
-}
-
-/* Style the search box inside the navigation bar */
-.topnav input[type=text] {
-  float: right;
-  padding: 6px;
-  border: none;
-  margin-top: 8px;
-  margin-right: 16px;
-  font-size: 17px;
-}
-
-/* When the screen is less than 600px wide, stack the links and the search field vertically instead of horizontally */
-@media screen and (max-width: 600px) {
-  .topnav a, .topnav input[type=text] {
-    float: none;
-    display: block;
-    text-align: left;
-    width: 100%;
-    margin: 0;
-    padding: 14px;
-  }
-  .topnav input[type=text] {
-    border: 1px solid #ccc;
-  }
-} ''')
-
-
-
-
-
-def html(file):
-	htmlfirst = open("website.html","w")
-	n=1
-	htmlStyle()
-	dic = omega(file)
-	info = alpha(file)
-	repetidos = beta(dic,info)
-	htmlfirst.write("""<!DOCTYPE html>
-<html>
-   <head>
-      <title>Enunciado 5</title>
-      <meta charset="UTF-8"/>
-      <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
-      <link href="styles/style.css" rel="stylesheet" type="text/css">
-   </head>
-   <body>
-      <h1> Enunciado 5, Trabalho Prático de Processamento de Linguagens</h1>
-      <p>Realizado por:</p>
-      <p> Duarte Oliveira A85517</p>
-      <p> Tiago Barata A81195</p>
-      <p> Simão Oliveira A57041</p>
-      <hr>
-    """
-    )
-
-	for elem in dic:
-
-		htmlfirst.write(
-			rf"""			
-			<h2>{elem}</h2>
-			<p>nº de elementos nesta categoria: {len(dic[elem])}</p>
-			"""
-			)
-
-		htmln = open(rf'categorias/{elem}.html',"w")
-		htmlrepeated = open(rf"categorias/{elem}2.html","w")
-		
-		htmln.write(
-rf"""<!DOCTYPE html>
-<html>
-   <head>
-      <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
-      <link href="../styles/style.css" rel="stylesheet" type="text/css">
-      <title>Info {elem} </title>
-""")
-		htmln.write(r"""<meta charset="UTF-8"/>
-</head>
-<body>
-   <div class="topnav">
-      <a class="active" href="../website.html"> Home </a>
-   </div>
-   </div>
-   </div>
-
-    """)
-
-		htmln.write(rf"""<h1>Lista de elementos da categoria {elem}</h1>
-<table>
-<tr>
-   <th>Elementos</th>
-   <th>Linha da ocorrência</th>
-</tr>
-<tr>""")
-# ______________________________________________________________________________________
-		#supostamente a cena de escrever no html com os repetidos esta aqui
-		htmlrepeated.write(
-rf"""<!DOCTYPE html>
-<html>
-   <head>
-      <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
-      <link href="../styles/style.css" rel="stylesheet" type="text/css">
-      <title>Info {elem} </title>
-""")
-		htmlrepeated.write(r"""<meta charset="UTF-8"/>
-</head>
-<body>
-   <div class="topnav">
-      <a class="active" href="../website.html"> Home </a>
-   </div>
-   </div>
-   </div>
-
-    """)
-
-		htmlrepeated.write(rf"""<h1>Lista de elementos da categoria {elem}</h1>
-<table>
-<tr>
-   <th>Elementos</th>
-   <th>Linha da ocorrência</th>
-</tr>
-<tr>""")
-		# DICIONARIO Repetidos k: ATOR V: 
-		#								DICIONARIO Elementos k : elementos V: lista das ocorrencias
-		# Repetidos{ATOR: Elementos{bruce willis:[4,3947,6451]}}
-		for categoria in repetidos[elem]:
-			tam = len(repetidos[elem][categoria])+1
-			htmlrepeated.write(rf"""
-						<td rowspan={tam}>
-	   {categoria} 
-	</td>""")
-			for ocorrencia in repetidos[elem][categoria]:
-				htmlrepeated.write(rf"""
-	<tr>
-	<td>{ocorrencia}</td>
-	</tr>
-					""")
-		#acaba aqui, o que está abaixo desta seccao esta bem
-		for listado, nrs in zip(dic[elem],info[elem]):
-			htmln.write(rf"""
-					<td>
-   {listado} 
-</td>
-<td>{nrs}</td>
-</tr>
-				""")
-		htmln.write(rf"""
-		</table>
-	</body>
-</html>""")
-			
-		htmlfirst.write(rf"""<a href="categorias/{elem}.html"> Informação {elem}</a>
-			<a href="categorias/{elem}2.html"> Repetidos </a>
-
-			<hr>
-			""")
-	htmlfirst.write(r"""</body>
-		</html>""")
-
-
-
 def main():
+	html.makeDirs()
+
 	print("Por favor, insira o nome do ficheiro a tratar:")
 	file = input()
 	lista = open(file).read().split("\n")
-	html(lista)
+
+	dic = omega(lista)
+	info = alpha(lista)
+	semRepetidos = beta(dic,info)
+
+	html.htmlStyle()
+	html.htmlHome(dic)
+	html.htmlRepetidos(dic,info)
+	html.htmlSemRepetidos(dic,semRepetidos)
+
 	url = 'website.html'
 
 	webbrowser.open(url, new=2)  # open in new tab
